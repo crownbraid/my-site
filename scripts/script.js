482e988b0d9820b973ba69849425e474ccea2821
oilylyme <elynorfreyss@gmail.com>
2017-02-21 15:38:15 -0500

    began instating 'back to menu' button

--- scripts/script.js
+++ scripts/script.js
@@ -24,11 +24,11 @@ $(function() {
     $(".navButton").click(function(e) {
         state_movenames = !state_movenames;
         e.preventDefault();
-        $(this).removeClass("hoveron").css("background-color", "black").css("opacity", ".6");
+        $(this).removeClass("hoveron").addClass("activeTab").css("background-color", "black").css("opacity", ".6");
         $("#background").animate({
             "opacity": ".2",
             "background-size": "104%",
-            "left": "-.2%"
+            "left": "-.2%",
         }, 4000);
         $("#navBackground, .hoveron").delay(900).animate({
             "left": "100%",
@@ -40,7 +40,8 @@ $(function() {
             "opacity": "1.0"
           }, 1000, function() {
             $(this).delay(50).animate({
-                "margin-left": "-31em",
+                "left": "-3",
+                "margin-left": ".2em"
             }, 850);
             $("#bodycontent").css('display', 'inline')
             .delay(800).animate({
@@ -55,5 +56,42 @@ $(function() {
             "left": "100%",
             "top": "0%"
         }, 1900);
+    });
+
+    $("#backToMenu").click(function(e) {
+        e.preventDefault();
+        $("#background").animate({
+            "opacity": ".6",
+            "background-size": "102%",
+            "left": "0",
+        }, 4000, function() {
+            state_movenames = !state_movenames;
+        });
+        $("#bodycontent").animate({
+                opacity: 0
+            }, 2000, function() {
+                $("#bodycontent").css('display', 'inline');
+        });
+        $(".activeTab").animate({
+            "left": "41%",
+            "margin-left": ".2em"
+        }, 850);
+        /*
+        $("#navBackground, .hoveron").delay(900).animate({
+            "left": "100%",
+            "opacity": "0"
+        }, 1000);
+
+        $("#name").delay(1050).animate({
+            "left": "2%",
+            "top": "85%"
+        }, 2600);
+        $("#lastName").delay(800).animate({
+            "left": "100%",
+            "top": "0%"
+        }, 1900);
+        */
+        $(".navButton").addClass("hoveron").removeClass("activeTab").css("background-color", "transparent").css("opacity", ".9");
+        console.log('great');
     });
 });
